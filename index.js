fd3064fa7dbad2f47f5746144b6a8b3ae202659f
Kantinan <kaseam008@gmail.com>
2022-12-21 09:53:22 +0700

    21/12/2022 9:46AM

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const express = require('express')
 const app = express()
+var publicDir = require('path').join(__dirname, '/public')
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static('public'));
-app.use('/images', express.static('images'));
+app.use(express.static(publicDir))
+// app.use(express.static(__dirname + '/public'));
+// app.use('/images', express.static('images'));
 
 app.get('/', (req, res) => {
     res.send("Hi, I'm Sheshhhhhhhh")
@@ -14,14 +15,13 @@ app.get('/about', (req, res) => {
     res.send('Hello from About Page')
 })
 
-app.get('/mainpage', (req, res) => {
-    res.sendFile(__dirname + '/html/Website.html')
+app.get('/mainpage', function(req, res){
+    res.sendFile(__dirname + '/public/Website.html')
 })
 
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/html/404 Not found.html')
+app.get('*', function(req, res){
+    res.sendFile(__dirname + '/public/404 Not found.html')
 })
-
 
 app.listen(3000, () => {
     console.log("Starting Server at Port [3000]")
